9a5ab2220cb108430ad9e116feb92b5194c796ea
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-23 04:53:29 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
-sudo https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -clean
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
 sudo rm /opt/webinoly/webinoly.conf
-sudo wget --no-check-certificate "https://raw.githubusercontent.com/bibicadotnet/Webinoly-custom/main/webinoly.conf" -O /opt/webinoly/webinoly.conf
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/webinoly.conf -O /opt/webinoly/webinoly.conf
 sudo stack -lemp -build=light
+
+
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
