4a099ecfda927d0c4754221efc8f9f796153c261
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-23 05:21:38 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade -y
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
+sudo sed -i "s|timezone:Asia/Ho_Chi_Minh|#timezone:Asia/Ho_Chi_Minh|" /opt/webinoly/webinoly.conf
+
+
+
+
+
 sudo rm /opt/webinoly/webinoly.conf
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/webinoly.conf -O /opt/webinoly/webinoly.conf
 sudo stack -lemp -build=light
@@ -15,3 +21,11 @@ sudo apt install htop -y
 sudo apt install screen -y
 sudo webinoly -verify
 sudo webinoly -info
+
+
+
+
+sudo sed -i "s|timezone:Asia/Ho_Chi_Minh|#timezone:Asia/Ho_Chi_Minh|" /opt/webinoly/webinoly.conf
+
+
+sed -i "s|CUSTOM1_LOG = .*|CUSTOM1_LOG = \"/home/nginx/domains/\*/log/access.log\"|" /etc/csf/csf.conf
