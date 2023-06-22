44ce9832d478e2966b5dfa8067eef3ad4dc26c61
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-23 06:08:28 +0700

    Update webinoly.sh

--- webinoly.sh
+++ webinoly.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+sudo apt install zip -y
+sudo apt install unzip -y
+sudo apt install htop -y
+sudo apt install screen -y
+screen -S bibica
 sudo apt update && sudo apt upgrade -y
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
 sudo sed -i "s|#timezone:America/Mexico_City|timezone:Asia/Ho_Chi_Minh|" /opt/webinoly/webinoly.conf
@@ -14,37 +19,9 @@ sudo sed -i "s|#header-xfo-url:<allow-from url>|header-xfo-url:off|" /opt/webino
 sudo sed -i "s|#php-max-mem:192|php-max-mem:512|" /opt/webinoly/webinoly.conf
 sudo sed -i "s|#php-max-time:60|php-max-time:6000|" /opt/webinoly/webinoly.conf
 sudo sed -i "s|#php-max-files:20|php-max-files:2000|" /opt/webinoly/webinoly.conf
-sudo sed -i "s|#header-xfo-url:<allow-from url>|header-xfo-url:off|" /opt/webinoly/webinoly.conf
-sudo sed -i "s|#header-xfo-url:<allow-from url>|header-xfo-url:off|" /opt/webinoly/webinoly.conf
-
-
-
-sed -i "s@nginx-ppa:stable@"  /opt/webinoly/webinoly.conf
-
-
-
-
-sed -i "s@^user.*@user = caddy@" /etc/php-fpm.d/www.conf
-
-sudo rm /opt/webinoly/webinoly.conf
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/webinoly.conf -O /opt/webinoly/webinoly.conf
 sudo stack -lemp -build=light
-
-
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
-sudo apt install zip -y
-sudo apt install unzip -y
-sudo apt install htop -y
-sudo apt install screen -y
 sudo webinoly -verify
 sudo webinoly -info
-
-
-
-
-sudo sed -i "s|timezone:Asia/Ho_Chi_Minh|#timezone:Asia/Ho_Chi_Minh|" /opt/webinoly/webinoly.conf
-
-
-sed -i "s|CUSTOM1_LOG = .*|CUSTOM1_LOG = \"/home/nginx/domains/\*/log/access.log\"|" /etc/csf/csf.conf
