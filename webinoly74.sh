6cebe48e7c1337984c6052819055b2f52cfe32ef
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-06-27 11:29:30 +0700

    Update webinoly74.sh

--- webinoly74.sh
+++ webinoly74.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-sudo apt update -y
+locale-gen en_US.UTF-8
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
 sudo apt update -y
 sudo apt install zip -y
 sudo apt install unzip -y
 sudo apt install htop -y
 sudo apt install screen -y
-locale-gen en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
 sudo rm /opt/webinoly/webinoly.conf
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_php74.conf -O /opt/webinoly/webinoly.conf
