72d1654abbc56a8642f03becb5fa37b71a6e2ebe
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-07 02:08:11 +0700

    Update VM.Standard.A1.Flex.sh

--- VM.Standard.A1.Flex.sh
+++ VM.Standard.A1.Flex.sh
@@ -11,15 +11,15 @@ sudo apt install screen -y
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
 sudo rm /opt/webinoly/webinoly.conf
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/webinoly_vm_standard_a1_flex.conf -O /opt/webinoly/webinoly.conf
-sudo stack -lemp -build=light
+sudo stack -lemp
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
 
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/php_vm_standard_a1_flex.ini -O /etc/php/7.4/fpm/php.ini
-sudo service php7.4-fpm restart
-sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/my_vm_standard_a1_flex.cnf -O /etc/mysql/my.cnf
-sudo service mysql restart
+#sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/php_vm_standard_a1_flex.ini -O /etc/php/7.4/fpm/php.ini
+#sudo service php7.4-fpm restart
+#sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/my_vm_standard_a1_flex.cnf -O /etc/mysql/my.cnf
+#sudo service mysql restart
 
 sudo apt update && sudo apt upgrade -y
 sudo webinoly -verify
